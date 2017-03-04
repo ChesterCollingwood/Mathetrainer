c6df2f13d3d88b1e2969955da964f9f6bed0d830
Max Kretzschmar <chester.collingwood@gmail.com>
2017-03-04 02:45:50 +0100

    View geupdatet, View mit Controller verknüpft

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,5 +8,29 @@ public class Controller {
 	
 	public Controller(View view) {
 		this.view = view;
+		
+		//weist den Buttons aus der View eine Methode beim Klicken zu
+		view.getDivisionBtn().addActionListener(listener -> showDivision());
+		view.getFractionBtn().addActionListener(listener -> showFraction());
+		view.getPercentBtn().addActionListener(listener -> showPercent());
+	}
+	
+	//Methoden zum Zeigen des jeweiligen ausgewählten Panels
+	public void showDivision() {
+		view.getMainPane().setVisible(false);
+		view.setSize(700, 700);
+		//view.getDivisionPane().setVisible(true);
+	}
+	
+	public void showFraction() {
+		view.getMainPane().setVisible(false);
+		view.setSize(700, 700);
+	//	view.getFractionPane().setVisible(true);
+	}
+	
+	public void showPercent() {
+		view.getMainPane().setVisible(false);
+		view.setSize(700, 700);
+	//	view.getPercentPane().setVisible(true);
 	}
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,9 +1,10 @@
 package view;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,16 +19,18 @@ public class View extends JFrame{
 		//erstellt alle sichtbaren Elemente
 		createVisibles();
 		
-		
 		//fügt die Panels dem Frame hinzu, zeigt erstmal nur das Main Panel
 		add(mainPane);
-		add(divisionPane);
+	/*	add(divisionPane);
 		add(percentPane);
 		add(fractionPane);
 		
 		divisionPane.setVisible(false);
 		percentPane.setVisible(false);
 		fractionPane.setVisible(false);
+		*/
+		//legt das Layout des Main Panels fest
+		mainPane.setLayout(new FlowLayout());
 		
 		//fügt die Buttons dem main Pane hinzu
 		mainPane.add(divisionBtn);
@@ -37,20 +40,44 @@ public class View extends JFrame{
 		//setzt die standard Schließen-Operation, Sichtbarkeit und Größe des Fensters
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		setSize(400, 400);
+		setSize(400, 350);
 	}
 	
 	public void createVisibles() {
+		Dimension buttonDimension = new Dimension(380, 100);
+		Font buttonFont = new Font("Arial", Font.PLAIN, 30);
+		
 		//Panels
 		mainPane = new JPanel();
+		mainPane.setOpaque(false);
+		
 		divisionPane = new JPanel();
-		fractionPane = new JPanel();
+		divisionPane.setOpaque(false);
+		
 		percentPane = new JPanel();
+		percentPane.setOpaque(false);
+		
+		fractionPane = new JPanel();
+		fractionPane.setOpaque(false);
 		
 		//Buttons
 		divisionBtn = new JButton("Division üben");
-		fractionBtn = new JButton("Brüche üben");
 		percentBtn = new JButton("Prozentrechnung üben");
+		fractionBtn = new JButton("Bruchrechnung üben");
+		
+		divisionBtn.setPreferredSize(buttonDimension);
+		percentBtn.setPreferredSize(buttonDimension);
+		fractionBtn.setPreferredSize(buttonDimension);
+		
+		divisionBtn.setFont(buttonFont);
+		percentBtn.setFont(buttonFont);
+		fractionBtn.setFont(buttonFont);
+		
+	}
+	
+	//Get Methoden für die Panels und die Buttons, zur Verknüpfung im Controller
+	public JPanel getMainPane() {
+		return mainPane;
 	}
 	
 	public JPanel getDivisionPane() {
@@ -63,5 +90,17 @@ public class View extends JFrame{
 	
 	public JPanel getPercentPane() {
 		return percentPane;
+	}
+	
+	public JButton getDivisionBtn() {
+		return divisionBtn;
+	}
+	
+	public JButton getFractionBtn() {
+		return fractionBtn;
+	}
+	
+	public JButton getPercentBtn() {
+		return percentBtn;
 	}
 }
