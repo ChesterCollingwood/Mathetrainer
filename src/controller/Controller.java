289b0a105f7d00a051d46ae287de137d52b52509
DangerDulf <dav.harder@gmail.com>
2017-03-14 01:00:16 +0100

    Funktionalitätsupdate
    Funktionalität der Rechenarten in das Programm implementiert
    Funktionalität der Buttons verknüpft

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
 	private String calcCase = null;
 	
 	public Controller(Preloader menu) {
-		//weist dem Controller den übergebenen Preloader zu
+		//weist dem Controller den Übergebenen Preloader zu
 		this.menu = menu;
 		
 		//initialisiert eine neue Main-View und das Data-Model
@@ -20,17 +20,89 @@ public class Controller {
 		data = new Data();
 		
 		//weist den Buttons einen ActionListener zu, setzt die gewünschte Rechenoperation
+		//DIVISION
 		menu.getDivisionBtn().addActionListener(e -> {
-			calcCase = "Division üben";
+			calcCase = "Division Üben";
 			showCalcFrame();
+		
+		//Erstellt bei pressen des Buttons eine neue Divisionsaufgabe
+			main.getGenerateBtn().addActionListener(f -> {
+				data.DivisionIni();
+				main.deleteAll();
+				main.setFirstNumber(data.getdDD());
+				main.setSecondNumber(data.getdDS());
+		
+			}); 
+		
+		//Prüft die Eingabe auf Richtigkeit und gibt einen entsprechenden Dialog zurück	
+			main.getCalculateBtn().addActionListener(g -> {
+				data.setdRQ(Integer.parseInt(main.getResult().getText()));
+				data.setdRR(Integer.parseInt(main.getRest().getText()));
+				if (data.getdQ() == data.getdRQ() && data.getdR() == data.getdRR()) {
+					main.showRight();
+				}
+				else {
+					main.showWrong();
+				}
+			});
 		});
+		
+		//BRUCHRECHNUNG
 		menu.getFractionBtn().addActionListener(e -> {
-			calcCase = "Bruchrechnung üben";
+			calcCase = "Bruchrechnung Üben";
 			showCalcFrame();
+		
+		//Erstellt bei pressen des Buttons einen neuen Bruch
+			main.getGenerateBtn().addActionListener(f -> {
+				data.FractionIni();
+				main.deleteAll();
+				main.setFractionUpperFirst(data.gettA());
+				main.setFractionLowerFirst(data.getbA());
+				data.FractionSimplify();
+				
+			});
+		
+		//Prüft die Eingabe auf Richtigkeit und gibt einen entsprechenden Dialog zurück	
+			main.getCalculateBtn().addActionListener(g -> {
+				data.setfRt(Integer.parseInt(main.getFractionUpperResult().getText()));
+				data.setfRb(Integer.parseInt(main.getFractionLowerResult().getText()));
+			
+				if (data.getfRt() == data.gettB() && data.getfRb() == data.getbB()) {
+					main.showRight();
+				}
+				else {
+					main.showWrong();
+				}
+				
+			});
+			
 		});
+		
+		//PROZETNRECHNUNG
 		menu.getPercentBtn().addActionListener(e -> {
-			calcCase = "Prozentrechnung üben";
+			calcCase = "Prozentrechnung Üben";
 			showCalcFrame();
+			
+		//Erstellt bei pressen des Buttons eine neue Prozentrechenraufgabe	
+			main.getGenerateBtn().addActionListener(f -> {
+				data.PercentageIni();
+				main.deleteAll();
+				main.setFirstNumber(data.getpF());
+				main.setSecondNumber(data.getpT());
+			
+		//Prüft die Eingabe auf Richtigkeit und gibt einen entsprechenden Dialog zurück		
+			});
+			main.getCalculateBtn().addActionListener(g -> {
+				data.setpP(Double.parseDouble(main.getPercentResult().getText()));
+				
+				if (data.getpP() == data.getpPP()) {
+					main.showRight();
+				}
+				else {
+					main.showWrong();
+				}
+			});
+				
 		});
 		
 		main.getReturnBtn().addActionListener(listener -> returnToPreload());
